44931f3eb4bfd5f2d08cc0ae2e0becb1ea1969c3
Guna Seelan <guna.nehru.13@gmail.com>
2016-01-10 17:56:50 +0530

    changed middleware

--- server.js
+++ server.js
@@ -2,16 +2,7 @@ var express = require('express');
 var app = express();
 var port =3000;
 
-var middleware = {
-	requestAuthentication : function (req, res, next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function (req, res, next){
-		console.log('Request: '+Date().toString()+req.method+' '+req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
